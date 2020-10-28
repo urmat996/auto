669bcf2d8efcccbad4b2cb3338382a7496e302c0
urmat996 <71040516+urmat996@users.noreply.github.com>
2020-10-29 04:20:07 +0600

    Some changes

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f99ee418da982001e8c4835",
+				"5f99ee418da982001e8c4837"
+			]
+		},
+		"5f99ee418da982001e8c4835": {
+			"id": "5f99ee418da982001e8c4835",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f99ee418da982001e8c4837": {
+			"id": "5f99ee418da982001e8c4837",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Text } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, Menu, Section } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section>
+			<Text margin="0px 0 24px 0" text-align="center" font="--headline2" md-font="--headline3">
+				Awesome Company
+			</Text>
+			<Menu
+				display="flex"
+				justify-content="center"
+				font="--base"
+				font-weight="700"
+				md-flex-direction="column"
+				md-align-items="center"
+			>
+				<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
+				<Override slot="link-active" color="--primary" />
+				<Override slot="item" padding="6px" />
+			</Menu>
+		</Section>
+	</Theme>;
+});
